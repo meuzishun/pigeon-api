a70891e8540e06f6dfed6855ef953871e5b4242f
Andrew Smith <meuzishun@gmail.com>
2024-06-21 15:05:36 -0400

    Add messages routes to room routes

--- routes/rooms.js
+++ routes/rooms.js
@@ -7,11 +7,13 @@ const {
   editRoom,
   deleteRoom,
 } = require('../controllers/roomController');
+const messageRoutes = require('./messages');
 
 router.get('/', authHandler, getRooms);
 router.get('/:roomId', authHandler, getRoom);
 router.post('/', authHandler, createRoom);
 router.put('/:roomId', authHandler, editRoom);
 router.delete('/:roomId', authHandler, deleteRoom);
+router.use('/:roomId/messages', authHandler, messageRoutes);
 
 module.exports = router;
