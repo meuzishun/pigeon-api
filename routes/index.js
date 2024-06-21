605bcc320596ea4eb18457d32a2c991f86791c34
Andrew Smith <meuzishun@gmail.com>
2024-06-21 15:04:12 -0400

    Remove messages routes from route index

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,12 @@
 const router = require('express').Router();
 const authRoutes = require('./auth');
 const roomsRoutes = require('./rooms');
-const messagesRoutes = require('./messages');
 const profileRoutes = require('./profile');
 const contactRoutes = require('./contacts');
 const userRoutes = require('./users');
 
 router.use('/auth', authRoutes);
 router.use('/rooms', roomsRoutes);
-router.use('/messages', messagesRoutes);
 router.use('/profile', profileRoutes);
 router.use('/contacts', contactRoutes);
 router.use('/users', userRoutes);
