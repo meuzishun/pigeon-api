c209306d613f9af8f5166de50d03c8d45c705ba8
Andrew Smith <meuzishun@gmail.com>
2025-02-02 19:16:31 -0500

    Adjust server.js to accommodate socket

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ require('./lib/generateKeyPair');
 const http = require('http');
 const { app } = require('./app');
 const { connectDB } = require('./config/database');
+const { initializeSocket } = require('./socket');
 
 const startApp = async () => {
   const server = http.createServer(app);
+  initializeSocket(server);
 
   await connectDB();
 
